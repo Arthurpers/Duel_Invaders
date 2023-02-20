efb4d4c202b814c51c5ad57aa3f198302fbfb263
arthur <arthur.pers@ensta-bretagne.org>
2023-02-20 14:51:11 +0100

    Client-serveur : Ping Pong

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example.duel_invaders {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.example.duel_invaders to javafx.fxml;
-    exports com.example.duel_invaders;
+    exports com.example.duel_invaders.servPattern;
+    opens com.example.duel_invaders.servPattern to javafx.fxml;
+    exports com.example.duel_invaders.client;
+    opens com.example.duel_invaders.client to javafx.fxml;
 }
